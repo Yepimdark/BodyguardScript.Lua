db2444febd6e6882393b0d2022d250e5367338a4
Yepimdark <curs3dbusiness@gmail.com>
2025-04-02 00:08:32 +0800

    Create TSB_Bodyguard.lua

---
+++ TSB_Bodyguard.lua
@@ -0,0 +1,14 @@
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+
+-- Create the bodyguard model
+local bodyguard = Instance.new("Model")
+bodyguard.Name = "Bodyguard"
+bodyguard.Parent = game.Workspace
+
+-- Create humanoid for the bodyguard
+local humanoid = Instance.new("Humanoid")
+humanoid.Parent = bodyguard
+
+-- Create the bodyguard's part
+local bodyguardPart = Instance.new
